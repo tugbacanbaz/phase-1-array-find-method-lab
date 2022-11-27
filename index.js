c206281b43a990bab415fceeed49b0d7364a4d11
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-11-27 20:11:31 +0300

    Complited Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"},
+    {year: "2015", result: "W"}
+    //...
+  ]
+
+  function superbowlWin(record){
+    const result=record.find(result=> result.result==='W');
+
+    if(result){
+        return result;
+    } else{
+        return undefined;
+    }
+  }
+
+  console.log(superbowlWin(record));
